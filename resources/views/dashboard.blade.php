18da4a4d011ce17a9b7d53eb0c231f64a9c7f656
marciogabriel1 <marciogabriel.dev@gmail.com>
2024-06-22 20:51:58 -0300

    atualização

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,13 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h2>Lista de Livros</h2>
                     <a href="{{ route('books.create') }}" class="btn btn-primary mb-3">Adicionar Livro</a>
-                    <table class="table table-bordered">
+                    <table class="table table-bordered border-primary">
                         <thead>
                             <tr>
                                 <th>ID</th>
