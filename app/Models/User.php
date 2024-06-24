0d9a8069e49aa7b6f1fe8348d1f408540ddc78e0
marciogabriel1 <marciogabriel.dev@gmail.com>
2024-06-24 17:28:44 -0300

    atualizacao

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,8 +13,6 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    // Outros métodos e atributos
-
     public function books()
     {
         return $this->hasMany(Book::class);
@@ -22,32 +20,17 @@ class User extends Authenticatable
 
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+    
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
